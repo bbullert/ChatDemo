cb6a2c341521b5b696f2ac7c6f693ccf191108f1
bullert <b.bullert5@gmail.com>
2021-10-04 18:35:01 +0200

    Add account controller with login & register view

---
+++ ChatDemo/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using ChatDemo.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> logger;
+        private readonly UserManager<AppUser> userManager;
+        private readonly SignInManager<AppUser> signInManager;
+
+        public AccountController(
+            ILogger<AccountController> logger,
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager)
+        {
+            this.logger = logger;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
