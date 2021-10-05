883936b1c3ffa02992e303262f3b606f9070d94d
bullert <b.bullert5@gmail.com>
2021-10-05 17:19:22 +0200

    Add models for FriendsController

---
+++ ChatDemo/Models/FriendsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ChatDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDemo.Models
+{
+    public class FriendsViewModel
+    {
+        public FriendsViewModel()
+        {
+            Friends = new List<AppUser>();
+        }
+
+        public ICollection<AppUser> Friends { get; set; }
+    }
+}
---
+++ ChatDemo/Models/UserSearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatDemo.Models
+{
+    public class UserSearchViewModel
+    {
+        public UserSearchViewModel()
+        {
+            Results = new List<UserSearchResult>();
+        }
+
+        public string Value { get; set; }
+
+        public ICollection<UserSearchResult> Results { get; set; }
+    }
+}
