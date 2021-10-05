ba75a4d2988df1af50d75561c23c43617abc461e
bullert <b.bullert5@gmail.com>
2021-10-05 19:24:20 +0200

    Create ChatList view component & add to main layout

---
+++ ChatDemo/ViewComponents/ChatListViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using ChatDemo.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace ChatDemo.ViewComponents
+{
+    public class ChatListViewComponent : ViewComponent
+    {
+        private readonly IChatRepository chatRepository;
+
+        public ChatListViewComponent(
+            IChatRepository chatRepository)
+        {
+            this.chatRepository = chatRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string signInUserId = UserClaimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var chats = chatRepository.GetUserChats(signInUserId);
+            foreach (var chat in chats)
+            {
+                chat.Name = chat.Users.FirstOrDefault(x => x.Id != signInUserId).UserName;
+            }
+
+            return View(chats);
+        }
+    }
+}
